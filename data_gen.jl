193f14c14c9d3f0cfbe6a7b9c1aeab56b2a99024
biryani <abhijith31792@gmail.com>
2020-06-04 20:05:13 -0600

    New stuff

--- data_gen.jl
+++ data_gen.jl
@@ -1,4 +1,5 @@
 include("Potts.jl")
+include("/home/abhijith/Dropbox/Code/GRISE_code/Potts_learning.jl")
 using IterTools:product
 
 
@@ -10,7 +11,8 @@ Terms = Dict{}()
 #The interaction strengths used in the paper are given below.
 #J = [0.2598910820966015  -0.653715969944616  0.07787582451255748 -0.3704026001241134 0.0397234544123 0.23441234]
 #For now we will generate them at random
-J =  2*0.7*rand(L) .-1
+J =  0.15*randn(L)
+#The larger the size of these numbers , more smaples will be required to leran the model. This can be seen from the IT bound
 println(J)
 
 for r in 1:L
@@ -37,7 +39,7 @@ writedlm( sample_path,hcat(samples...))
 #Generating another set of samples for testing the trained model.
 
 
-s = raw_sampler_potts(F, n_samples, false)
+s = raw_sampler_potts(F, 10^6, false)
 samples2=[2*a .- 3  for a in s]
 sample_path ="samples_test.csv"
 writedlm( sample_path,hcat(samples2...))
---
+++ learn.py
@@ -0,0 +1,74 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from NN_learn import *
+import os, shutil
+
+from tensorflow.keras.layers import Activation
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import get_custom_objects
+from tensorflow.keras import layers
+from tensorflow import keras
+def swish(x):
+    return x * K.sigmoid(x)
+
+get_custom_objects().update({'swish': Activation(swish)})
+
+
+samples =  np.loadtxt( "samples.csv" ).T
+
+samples_test = np.loadtxt( "samples_test.csv" ).T
+N=samples.shape[1]
+N_samples = 10**6
+print("(n,p) = ",(N , N_samples))
+
+
+
+
+
+schedule =  tf.keras.optimizers.schedules.InverseTimeDecay(
+0.01,   decay_steps= 10000,
+decay_rate=0.2, staircase=True)
+width=15
+
+model = tf.keras.Sequential()
+model.add(layers.Dense(width,activation= swish, input_shape=(N-1,)))
+model.add(layers.Dense(width, activation=swish))
+#model.add(layers.Dense(width, activation=swish))
+model.add(layers.Dense(1))
+
+
+#model.add(layers.Dense(1 , input_shape=(N-1,)))
+model.summary()
+H = train_NN_ising(samples, model, epochs = 100, batch_size=2000, eta = schedule)
+
+
+print("Training done! Sampling from the learned model")
+NN_samples = NN_MCMC(H, N)
+
+ns_list = [4*10**4, 10**5, 4*10**5, 10**6]
+base_line = []
+TV_err = []
+
+print("Sampling done! Calculating TV")
+
+for ns in ns_list:
+    base_line.append( TVD(samples[:ns,:], samples_test[:ns, :],  ns) )
+    TV_err.append( TVD(samples_test[:ns,:], NN_samples[:ns, :], ns) )
+
+
+
+plt.rc('font', family='serif')
+plt.plot(ns_list, TV_err , '-+', label =  "$n=10^6$,NN_GRISE")
+
+plt.plot(ns_list, base_line, '--', label =  "Sampling error")
+
+
+plt.xlabel("Number of testing samples")
+plt.ylabel("TVD")
+plt.xscale('log')
+plt.yscale('log')
+plt.yticks([0.1,1])
+
+plt.legend(bbox_to_anchor=(0, 0), loc=3, prop={'size': 7})
+plt.show()
